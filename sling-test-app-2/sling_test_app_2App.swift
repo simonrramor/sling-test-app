78daf7cffcbe40bff02fd367faa996c7ee453d27
simon amor <simonrramor@gmail.com>
2026-01-14 16:00:19 +0000

    Initial Commit

---
+++ sling-test-app-2/sling_test_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  sling_test_app_2App.swift
+//  sling-test-app-2
+//
+//  Created by Simon Amor on 14/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct sling_test_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
